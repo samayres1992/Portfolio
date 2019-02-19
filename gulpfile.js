81e97622f244959655a7bad37249577ca0f9f63c
samayres1992 <samayres1992@gmail.com>
2019-02-19 13:06:38 +0000

    added gulp compiling

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var update = require('gulp-update')();
+var scss = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('css', function () {
+    return gulp.src([
+        './resources/scss/app.scss',
+    ])
+    .pipe(scss())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('js', function () {
+    return gulp.src([
+        './resources/js/particles.min.js',
+        './resources/js/fontawesome.min.js',
+        './resources/js/main.js',
+    ])
+    .pipe(concat(
+        'app.js'
+    ))
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('update', function update () {
+    gulp.watch('./package.json').on('change', function (file) {
+        update.write(file);
+    });
+});
+
+gulp.task('default', gulp.parallel('css', 'js', 'update'));
