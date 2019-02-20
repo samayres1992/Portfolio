caeabb186576eb933146062ae0f8ece2e5620e08
samayres1992 <samayres1992@gmail.com>
2019-02-20 03:07:06 +0000

    fixed particles

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,35 @@ var update = require('gulp-update')();
 var scss = require('gulp-sass');
 var concat = require('gulp-concat');
 
-gulp.task('css', function () {
+gulp.task('compile-css', function () {
     return gulp.src([
-        './resources/scss/app.scss',
+        './node_modules/normalize.css/normalize.css',
+        './node_modules/bulma/bulma.sass',
+        './node_modules/bulma-extensions/dist/css/bulma-extensions.min.css',
+        './resources/scss/main.scss',
     ])
     .pipe(scss())
+    .pipe(gulp.dest('./resources/compiled'));
+});
+
+gulp.task('css', function () {
+    return gulp.src([
+        './resources/compiled/normalize.css',
+        './resources/compiled/bulma.css',
+        './resources/compiled/bulma-extensions.min.css',
+        './resources/compiled/main.css',
+    ])
+    .pipe(concat(
+        'app.css'
+    ))
     .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('js', function () {
     return gulp.src([
-        './resources/js/particles.min.js',
-        './resources/js/fontawesome.min.js',
-        './node_modules/bulma-extensions/bulma-carousel/src/js/index.js',
+        './node_modules/@fortawesome/fontawesome-free/js/all.min.js',
+        './node_modules/bulma-extensions/dist/js/bulma-extensions.min.js',
+        './node_modules/particlesjs/dist/particles.min.js',
         './resources/js/main.js',
     ])
     .pipe(concat(
@@ -30,4 +46,4 @@ gulp.task('update', function update () {
     });
 });
 
-gulp.task('default', gulp.series('css', 'js', 'update'));
+gulp.task('default', gulp.series('compile-css', 'css', 'js', 'update'));
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,75 +1,75 @@
+/* Particles */
+
 particlesJS(
   "galaxy", {
-   "particles": {
-    "number": {
-      "value": 2,
-      "density": {
-        "enable": true,
-        "value_area": 50
-      }
-    },
-     
-     
-    "color": {
-      "value": ["#de0061", "#00a2a5", "#a8c94d"]
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 4,
-        "color": "rgba(119, 197, 187, 0.38)"
+    "particles": {
+      "number": {
+        "value": 2,
+        "density": {
+          "enable": true,
+          "value_area": 50
+        }
       },
-    },
+      "color": {
+        "value": ["#de0061", "#00a2a5", "#a8c94d"]
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 4,
+          "color": "rgba(119, 197, 187, 0.38)"
+        },
+      },
       "opacity": {
-      "value": 0.8,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 10,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 5,
-      "random": true,
-      "anim": {
-        "enable": false,
-      }
-    },
-    "line_linked": {
-      "enable": false
-    },
-    "move": {
-      "enable": true,
-      "speed": 1,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out"
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
+        "value": 0.8,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 10,
+          "opacity_min": 0.1,
+          "sync": false
+        }
       },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+        }
       },
-      "resize": true
-    },
-    "modes": {
-      "push": {
-        "particles_nb": 10
+      "line_linked": {
+        "enable": false
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out"
       }
-    }
-  },
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "push": {
+          "particles_nb": 10
+        }
+      }
+    },
 
     "push": {
       "particles_nb": 2
     },
-  "retina_detect": true
+    "retina_detect": true
 });
